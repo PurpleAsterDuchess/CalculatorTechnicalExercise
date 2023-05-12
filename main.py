766fc1291473fe22167e11c24bc4696d51883cfe
PurpleAsterDuchess <126280657+PurpleAsterDuchess@users.noreply.github.com>
2023-05-12 20:25:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# get the code from calculator.py
+# get the code from functions.py
 from functions import *
 
 repeat = True
@@ -8,11 +8,14 @@ while repeat:
     print("Enter 0 to close\n"+
                 "Enter 1 for the normal calculator \n"+
                 "Enter 2 for the conversion calculator")
-    option = int(input())
+    option = input()
     clear()
-    if option == 1:
+    if option == "1":
         calc()
-    elif option == 2:
+    elif option == "2":
         conversion()
-    else:
+    elif option == "0":
         repeat = False
+    else:
+        print("That was not a valid input.")
+        sleep(delay)
