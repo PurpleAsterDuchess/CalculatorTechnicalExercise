43c4dba93280ef26ecd664c4529767a3df415b2c
PurpleAsterDuchess <126280657+PurpleAsterDuchess@users.noreply.github.com>
2023-05-13 17:18:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ while repeat:
     clear()  
     print("Enter 0 to close\n"+
                 "Enter 1 for the normal calculator \n"+
-                "Enter 2 for the conversion calculator")
+                "Enter 2 for the conversion calculator\n"+
+                "Enter 3 for the base calculator")
     option = input()
     clear()
-    if option == "1":
-        calc()
+    if option == "0":
+      repeat = False
+    elif option =="1":
+      calc()
     elif option == "2":
-        conversion()
-    elif option == "0":
-        repeat = False
+      conversion()
+    elif option == "3":
+      convertBase()
     else:
         print("That was not a valid input.")
         sleep(delay)
